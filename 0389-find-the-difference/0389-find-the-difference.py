81b4d41b020488a588151a71f3a294566e81d78b
MATHU MATHI M <153843978+MATHU-MATHI-M@users.noreply.github.com>
2026-06-24 21:27:13 +0530

    Time: 0 ms (100%), Space: 12.3 MB (64.9%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def findTheDifference(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
         c =0
         for cs in s:
             c ^= ord(cs)
