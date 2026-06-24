9eedbf80ee620ddda5046ae9950b47431ec209f5
MATHU MATHI M <153843978+MATHU-MATHI-M@users.noreply.github.com>
2026-06-24 21:26:46 +0530

    Time: 4 ms (45.6%), Space: 12.4 MB (64.9%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        c =0
+        for cs in s:
+            c ^= ord(cs)
+        for ct in t:
+            c ^= ord(ct)
+        return chr(c)
+        
